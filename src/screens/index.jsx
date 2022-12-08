7cb9172bae04747ebc0f613f1b1b89389591c8bc
M Abrar Khalid <abrarprince471@gmail.com>
2022-12-08 16:28:44 +0500

    contact form done

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -24,6 +24,45 @@ export default function Index() {
       {nextpagelink()}
       {aboutjseven()}
       {ourproject()}
+      <section className="formsection">
+        <div className="formsection__content">
+          <div className="formsection__content__all__input">
+            <div className="formsection__content__input">
+              <input type="Name" name="Name" id="Name" placeholder="Name" />
+            </div>
+          </div>
+          <div className="formsection__content__all__input">
+            <div className="formsection__content__input">
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+          </div>
+          <div className="formsection__content__all__input">
+            <div className="formsection__content__input">
+              <input type="tel" name="tel" id="tel" placeholder="Number" />
+            </div>
+          </div>
+          <div className="formsection__content__all__input">
+            <div className="formsection__content__input">
+              <input type="Text" name="text" id="text" placeholder="City" />
+            </div>
+          </div>
+          <div className="formsection__content__all__msg">
+            <div className="formsection__content__msg">
+              <textarea
+                type="text"
+                name="text"
+                id="text"
+                placeholder="Message"
+                cols={30}
+                rows={8}
+              />
+            </div>
+          </div>
+          <div className="formsection__content__all__button">
+            <button>Submit</button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
